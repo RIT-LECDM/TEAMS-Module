1bfb2b2a108f1401b5a32b4080d3007c78529d5d
Shayde <sln7755@rit.edu>
2015-05-21 11:06:54 -0400

    working on twp calc utility functions

---
+++ TEAMSModule/Plugin/TEAMSPlugin/TWPCalc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TEAMSModule
+{
+    class TWPCalc
+    {
+        private const int VALUE = 12; 
+        public TWPCalc() { }
+
+        public double pulseTRF(double time)
+        {
+            double calcE = 1 - Math.Pow(Math.E, (-time / VALUE));
+            return VALUE * calcE;
+        }
+
+        public double continousTRF(double time)
+        {
+            double calcE = 1 - Math.Pow(Math.E, (-time / VALUE));
+            return (VALUE * time) - (VALUE * VALUE * calcE);
+        }
+
+        public double servicelifeTRF(double time, int serviceLife)
+        {
+            if (time <= serviceLife)
+            {
+                // just use continous
+                return continousTRF(time);
+            }
+            else
+            {
+                int c_sl = VALUE * serviceLife;
+                double e_1 = Math.Pow(Math.E, (-time/VALUE));
+                double e_2 = Math.Pow(Math.E, (serviceLife / VALUE) - 1);
+
+                return (c_sl) - (VALUE * VALUE * e_1 * e_2);
+            }
+        }
+    }
+}
